1ed9326ba32c0c44909eb6695551b9ebb5949a6d
Antonio Alan <tonio.alan07@gmail.com>
2022-08-08 10:27:29 -0300

    bodyParser installation and post form implementation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser')
 const Sequelize = require('sequelize');
 
 //Conexão com o banco de dados MySql
@@ -13,9 +14,17 @@ const sequelize = new Sequelize('test', 'root', '123456',{
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get('/cad', function(req,res){
     res.render('form')
+})
+
+app.post('/add', function(req,res){
+
+    res.send("Text: " +req.body.title+" Content: " + req.body.content)
 })
 
 app.listen(4001, function(){
